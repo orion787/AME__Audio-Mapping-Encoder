cfb226abef91b8842d283793bd20e154897e84ab
orion787 <116973576+orion787@users.noreply.github.com>
2024-11-22 15:56:05 +0300

    Update flac_reader.hpp

--- src/readers/flac_reader.hpp
+++ src/readers/flac_reader.hpp
@@ -5,20 +5,35 @@
 #include <fstream>
 #include <stdexcept>
 #include <sndfile.h>
+#include <string>
+#include <locale>
+#include <codecvt>
 
+// Конвертация UTF-8 -> UTF-16
+static std::wstring utf8_to_wstring(const std::string& utf8) {
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    return converter.from_bytes(utf8);
+}
 
 class FLACReader {
 public:
     std::vector<std::complex<double>>* read(const std::string& filename) {
-        // Открываем файл с помощью libsndfile
+#ifdef _WIN32
+        // Преобразуем UTF-8 путь в wstring для Windows
+        std::wstring wide_filename = utf8_to_wstring(filename);
+        SF_INFO sfInfo;
+        SNDFILE* file = sf_wchar_open(wide_filename.c_str(), SFM_READ, &sfInfo);
+#else
         SF_INFO sfInfo;
         SNDFILE* file = sf_open(filename.c_str(), SFM_READ, &sfInfo);
+#endif
         if (!file) {
             throw std::runtime_error("Не удалось открыть FLAC файл с помощью libsndfile.");
         }
 
         // Проверка формата файла (должен быть PCM)
-        if (sfInfo.format & SF_FORMAT_TYPEMASK != SF_FORMAT_FLAC || (sfInfo.format & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16) {
+        if ((sfInfo.format & SF_FORMAT_TYPEMASK) != SF_FORMAT_FLAC ||
+            (sfInfo.format & SF_FORMAT_SUBMASK) != SF_FORMAT_PCM_16) {
             sf_close(file);
             throw std::runtime_error("Неподдерживаемый формат файла.");
         }
