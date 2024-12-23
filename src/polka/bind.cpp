8be71eb13622353af5e9608034e5011aafe86963
orion787 <116973576+orion787@users.noreply.github.com>
2024-12-23 13:41:09 +0300

    Create polka folder

---
+++ src/polka/bind.cpp
@@ -0,0 +1,37 @@
+#include "FilePasswordProcessor.cpp"
+#include <iostream>
+#include "SHA512ID.cpp"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+
+std::string fileSHAPassword(std::wstring filePath) {
+    try {
+        size_t blockSize = 16384;
+        size_t numThreads = 8;
+        size_t passwordLength = 16;
+
+        FilePasswordProcessor processor(filePath, blockSize, numThreads);
+        std::string password = processor.processFile(passwordLength);
+
+        return password;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+std::string fileSHAID(std::string filePath){
+    try{
+        std::string hash = computeFileHash(filePath);
+        return hash;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+
+PYBIND11_MODULE(polka, m) {
+    m.def("filePassword", &fileSHAPassword, "Convert file to password");
+    m.def("fileID", &fileSHAID, "Convert file to ID");
+}
