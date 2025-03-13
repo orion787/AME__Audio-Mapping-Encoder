66df89ea3cf77784886296dd19f551187d910d53
orion787 <116973576+orion787@users.noreply.github.com>
2025-03-13 20:28:02 +0300

    Add initial binding for fibuki module
    Add initial binding for fibuki python module

---
+++ src/fibuki/bind.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "Readers/ReaderFactory.cpp"
+#include "FourierTransforms/FFTBasic.cpp"
+#include "FourierTransforms/FFT_AVX256.cpp"
+#include "FourierTransforms/FFT_ExtraOptimized.cpp"
+#include "Generators/DefaultIDMikuzator.cpp"
+#include "Generators/DefaultPasswordMikuzator.cpp"
+#include "Processors/Processors.cpp"
+
+using namespace std;
+namespace py = pybind11;
+
+
+string generateID(string file_path) {
+    unique_ptr<Reader> reader = ReaderFactory::createReader(string(file_path));
+    if (!reader) {
+        throw runtime_error("Unsupported file format");
+    }
+
+    unique_ptr<vector<float>> audioData = reader->read(string(file_path));
+    if (!audioData || audioData->empty()) {
+        throw runtime_error("Error loading audio file or file is empty");
+    }
+
+    unique_ptr<FourierTransform> fft = unique_ptr<FourierTransform>(createFFTBasic());
+    vector<float> fftAmplitudes;
+    fft->compute(*audioData, fftAmplitudes);
+
+    DefaultIDMikuzator idGen;
+    IDProcessor idProc(&idGen);
+    return idProc.process(fftAmplitudes);
+}
+
+string generatePassword(string file_path) {
+    unique_ptr<Reader> reader = ReaderFactory::createReader(string(file_path));
+    if (!reader) {
+        throw runtime_error("Unsupported file format");
+    }
+
+    unique_ptr<vector<float>> audioData = reader->read(string(file_path));
+    if (!audioData || audioData->empty()) {
+        throw runtime_error("Error loading audio file or file is empty");
+    }
+
+    unique_ptr<FourierTransform> fft = unique_ptr<FourierTransform>(createFFTBasic());
+    vector<float> fftAmplitudes;
+    fft->compute(*audioData, fftAmplitudes);
+
+    DefaultPasswordMikuzator pwdGen;
+    PasswordProcessor pwdProc(&pwdGen);
+    return pwdProc.process(fftAmplitudes);
+}
+
+
+PYBIND11_MODULE(fibuki, m) {
+    m.doc() = "Модуль для генерации ID и пароля из аудиофайлов";
+    m.def("audioID", &generateID, "Генерация ID по аудиофайлу", py::arg("file_path"));
+    m.def("audioPassword", &generatePassword, "Генерация пароля по аудиофайлу", py::arg("file_path"));
+}
